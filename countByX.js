fac5335b82cba08e5930eeeb98d7e24be105926c
Ross Brown <111411908+ross-brown@users.noreply.github.com>
2022-09-23 18:38:15 -0500

    Create countByX.js

---
+++ countByX.js
@@ -0,0 +1,17 @@
+
+/* DESCRIPTION:
+Create a function with two arguments that will return an array of the first n multiples of x.
+
+Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+Return the results as an array or list ( depending on language ). */
+
+// ANSWER //
+
+function countBy(x, n) {
+  let arr = [];
+  for (let i = x; i <= n*x; i+=x) {
+    arr.push(i);
+  }
+  return arr;
+}
