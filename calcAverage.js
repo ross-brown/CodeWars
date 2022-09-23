67e6d60b188654ff60385f3517dc7504b9df39e5
Ross Brown <111411908+ross-brown@users.noreply.github.com>
2022-09-23 18:43:44 -0500

    Create calcAverage.js

---
+++ calcAverage.js
@@ -0,0 +1,15 @@
+
+/* DESCRIPTION:
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0. */
+
+// ANSWER //
+
+function find_average(array) {
+  if (array.length === 0) {
+    return 0;
+  } else {
+  return array.reduce((a, b) => a + b, 0) / array.length;
+  }
+}
