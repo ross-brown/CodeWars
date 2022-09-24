670bff34ad937f15d7d47b45dc937f48304351fe
Ross Brown <111411908+ross-brown@users.noreply.github.com>
2022-09-24 14:51:10 -0500

    Create sumArrExcept.js

---
+++ sumArrExcept.js
@@ -0,0 +1,19 @@
+
+/* DESCRIPTION:
+Task
+Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+Mind the input validation. */
+
+
+// ANSWER //
+
+function sumArray(array) {
+  if (array === null || array === undefined || array.length < 3) {
+    return 0;
+  } else {
+    return array.sort(function(a, b) {return a-b}).slice(1, -1).reduce((a, b) => a+b)
+  }
+}
