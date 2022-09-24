15a8b8fbbba5d3998d79f3c19ef3221b07b55cf0
Ross Brown <111411908+ross-brown@users.noreply.github.com>
2022-09-24 15:07:20 -0500

    Create buildTower.js

---
+++ buildTower.js
@@ -0,0 +1,32 @@
+/* DESCRIPTION
+Build Tower
+Build a pyramid-shaped tower given a positive integer number of floors. A tower block is represented with "*" character.
+
+For example, a tower with 3 floors looks like this:
+
+[
+  "  *  ",
+  " *** ", 
+  "*****"
+]
+And a tower with 6 floors looks like this:
+
+[
+  "     *     ", 
+  "    ***    ", 
+  "   *****   ", 
+  "  *******  ", 
+  " ********* ", 
+  "***********"
+] */
+
+
+// ANSWER //
+
+function towerBuilder(nFloors) {
+  let tower = [];
+  for (let i = 0; i < nFloors; i++) {
+    tower.push(' '.repeat(nFloors - i - 1) + '*'.repeat((i*2+1)) + ' '.repeat(nFloors - i - 1))
+  }
+  return tower;
+}
