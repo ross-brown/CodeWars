0ed9d22098c1650346f92b310b6db14e64bcc2b7
Ross Brown <111411908+ross-brown@users.noreply.github.com>
2022-09-24 12:24:34 -0500

    Create makeNegative.js

---
+++ makeNegative.js
@@ -0,0 +1,10 @@
+
+/* DESCRIPTION:
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative? */
+
+
+// ANSWER //
+
+function makeNegative(num) {
+  return num > 0 ? -num : num;
+}
