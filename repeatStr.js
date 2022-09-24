0ca6527a9a0388f647bf644e99e422996fcf9641
Ross Brown <111411908+ross-brown@users.noreply.github.com>
2022-09-24 13:33:46 -0500

    Create repeatStr.js

---
+++ repeatStr.js
@@ -0,0 +1,10 @@
+
+/* DESCRIPTION:
+Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times. */
+
+
+// ANSWER //
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
