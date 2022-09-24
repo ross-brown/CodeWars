9b31ba5d253e2d7d5eebd85926b9e9c53766aca1
Ross Brown <111411908+ross-brown@users.noreply.github.com>
2022-09-24 14:08:04 -0500

    Create numToReverseArr.js

---
+++ numToReverseArr.js
@@ -0,0 +1,11 @@
+
+/* DESCRIPTION:
+Convert number to reversed array of digits
+Given a random non-negative number, you have to return the digits of this number within an array in reverse order. */
+
+
+// ANSWER //
+
+function digitize(n) {
+  return n.toString().split('').reverse().map(Number);
+}
