8d25f2dbc185757ecb7ed093be43ddc77b7c57e2
Ross Brown <111411908+ross-brown@users.noreply.github.com>
2022-09-24 15:33:48 -0500

    Create rockPaperScissors.js

---
+++ rockPaperScissors.js
@@ -0,0 +1,23 @@
+
+/* DESCRIPTION:
+Rock Paper Scissors
+Let's play! You have to return which player won! In case of a draw return Draw!. */
+
+
+// ANSWER //
+
+const rps = (p1, p2) => {
+  if (p1 === p2) {
+    return "Draw!"
+  } else if (p1 === 'rock' && p2 === 'scissors') {
+    return "Player 1 won!"
+  } else if (p1 === 'scissors' && p2 === 'paper') {
+    return "Player 1 won!"
+  } else if (p1 === 'paper' && p2 === 'rock') {
+    return "Player 1 won!"
+  } else {
+    return "Player 2 won!"
+  }
+};
+  
+  
