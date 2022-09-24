547d976d4836a0ce042762b4d24cc3cf3677359f
Ross Brown <111411908+ross-brown@users.noreply.github.com>
2022-09-24 13:35:36 -0500

    Create numToStr.js

---
+++ numToStr.js
@@ -0,0 +1,12 @@
+
+/* DESCRIPTION:
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know? */
+
+
+// ANSWER //
+
+function numberToString(num) {
+  return num.toString();
+}
