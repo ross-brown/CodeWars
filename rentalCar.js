a68e32102b18d240ce0621a57f60e2bccdaa3ac5
Ross Brown <111411908+ross-brown@users.noreply.github.com>
2022-10-05 11:16:31 -0500

    Create rentalCar.js

---
+++ rentalCar.js
@@ -0,0 +1,28 @@
+
+/* DESCRIPTION:
+After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
+
+You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+
+Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+
+Write a code that gives out the total amount for different days(d). */
+
+
+// ANSWER //
+
+function rentalCarCost(d) {
+  let rate = d * 40
+  if (d >= 7) {
+    return rate - 50
+  } else if (d >= 3) {
+    return rate - 20
+  } else {
+    return rate
+  }
+}
+
+
+//$40 a day 
+// days >= 7 then -$50 off
+// days >= 3 then -$20 off
