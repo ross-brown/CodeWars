576815c3b961793c9f9ea9aa3602686ad9375710
Ross Brown <111411908+ross-brown@users.noreply.github.com>
2022-11-01 10:33:54 -0500

    Create pipeFix.js

---
+++ pipeFix.js
@@ -0,0 +1,32 @@
+
+/* DESCRIPTION:
+Issue
+Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
+
+The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
+
+Pipes list is correct when each pipe after the first index is greater (+1) than the previous one, and that there is no duplicates.
+
+Task
+Given the a list of numbers, return a fixed list so that the values increment by 1 for each index from the minimum value up to 
+the maximum value (both included).
+
+Example
+Input: 1,3,5,6,7,8  Output: 1,2,3,4,5,6,7,8 */
+
+
+// ANSWER //
+
+function pipeFix(numbers){
+  //declare max and min as first and last number in array
+  let min = numbers[0];
+  let max = numbers[numbers.length - 1];
+  //declare output array
+  let output = [];
+  //loop through min to max and push numbers to output array
+  for (let i = min; i <= max; i++) {
+    output.push(i);
+  };
+  //return array
+  return output;
+};
