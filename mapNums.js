5c73b3e63478c34ff5d54be9d099baef1ae34d11
Ross Brown <111411908+ross-brown@users.noreply.github.com>
2022-11-06 18:36:50 -0600

    Create mapNums.js

---
+++ mapNums.js
@@ -0,0 +1,16 @@
+
+/* DESCRIPTION:
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6] */
+
+
+// ANSWER //
+
+function maps(x) {
+  //use map method to create new array with each number doubled 
+  return x.map(num => num * 2);
+};
+
