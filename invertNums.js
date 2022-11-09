124176bf20cff3345c87c826f52f9e5dd36923d0
Ross Brown <111411908+ross-brown@users.noreply.github.com>
2022-11-08 23:48:09 -0600

    Create invertNums.js

---
+++ invertNums.js
@@ -0,0 +1,16 @@
+
+/* DESCRIPTION:
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+You can assume that all values are integers. Do not mutate the input array/list. */
+
+
+// ANSWER //
+
+function invert(array) {
+  //return input array with each num inversed 
+  return array.map(num => -num);
+}
